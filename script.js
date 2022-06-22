357d8485ea087d250b86294c226f41c2e349a454
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-06-22 16:35:04 -0400

    making script code prettier

--- script.js
+++ script.js
@@ -1,58 +1,41 @@
+document.getElementById("myBTN").onclick = function () {
+  var textInp = document.getElementById("anything").value;
 
-//when a user click on the button, addeventlisthener means listhen for the events of a user
-//
-document.getElementById("myBTN").onclick = function() {
-    var textInp = document.getElementById('anything').value;
+  var li = document.createElement("li");
+  var ul = document.getElementById("unorderlist");
 
-var li = document.createElement('li'); 
- var ul = document.getElementById("unorderlist");
+  var NodeBtn = document.getElementsByTagName("li");
+  var t = document.createTextNode(textInp);
 
+  li.appendChild(t);
+  ul.appendChild(li);
 
-var NodeBtn = document.getElementsByTagName('li');
-var t = document.createTextNode(textInp);
-
-li.appendChild(t);
-ul.appendChild(li);
-
-for(let i = 0; i< NodeBtn.length; i++){
+  for (let i = 0; i < NodeBtn.length; i++) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     NodeBtn[i].appendChild(span);
+  }
+  var close = document.getElementsByClassName("close");
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+};
 
+function myFunction() {
+  const list = document.getElementById("unorderlist");
+  list.removeChild(list.firstElementChild);
 }
-var close = document.getElementsByClassName("close");
-for(let i =0; i<close.length; i++){
-    close[i].onclick = function(){
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
+document.getElementById("myClear").onclick = function () {
+  var myList = document.getElementById("unorderlist");
+  const inside = document.querySelectorAll("#anything");
 
-
-}
-
-
-
-
-}
-
-
-function myFunction(){
-    const list = document.getElementById('unorderlist');
-    list.removeChild(list.firstElementChild);
-}
-
-document.getElementById("myClear").onclick = function() {
-
-
-    var myList = document.getElementById('unorderlist');
-    const inside = document.querySelectorAll('#anything');
-
-
-   
-    myList.innerHTML = '';
-    inside.forEach(input => {
-        input.value = '';
-      });
-}
+  myList.innerHTML = "";
+  inside.forEach((input) => {
+    input.value = "";
+  });
+};
