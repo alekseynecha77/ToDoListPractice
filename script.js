dee8c38dd64d8e8918f5cfa21a3093b8e73ff0a5
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-06-27 20:30:39 -0400

    adding edit button

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+const editBTN = document.getElementById('unorderlist');
+
+
 document.getElementById("myBTN").onclick = function () {
   var textInp = document.getElementById("anything")
 
   var ul = document.getElementById("unorderlist");
 
   var NodeBtn = document.getElementsByTagName("li");
+ 
 
-  ul.insertAdjacentHTML(
-    'afterbegin',
-    `<li>${textInp.value}</li>`
+  var button = document.createElement("button");
+  var li = document.createElement('li');
+li.innerHTML +=  `<li>${textInp.value}</li>`
+  button.innerHTML = "edit";
 
-  );
+  li.appendChild(button);
+  ul.appendChild(li);
+
 
 
   for (let i = 0; i < NodeBtn.length; i++) {
@@ -28,6 +35,34 @@ document.getElementById("myBTN").onclick = function () {
   }
 };
 
+editBTN.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    const button = e.target;
+    const li = button.parentNode;
+    const ul = li.parentNode;
+    if (button.textContent === 'remove') {
+      ul.removeChild(li);
+    } else if (button.textContent === 'edit') { 
+      const span = li.firstElementChild;
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.value = span.textContent;
+      li.insertBefore(input, span);
+      li.removeChild(span);
+      button.textContent = 'save';
+    } else if (button.textContent === 'save') { 
+      const input = li.firstElementChild;
+      const span = document.createElement('span');
+      span.textContent = input.value;
+      li.insertBefore(span, input);
+      li.removeChild(input);
+      button.textContent = 'edit';
+    }
+  }
+});  
+
+    
+ 
 function myFunction() {
   const list = document.getElementById("unorderlist");
   list.removeChild(list.firstElementChild);
