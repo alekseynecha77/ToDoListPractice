03ff09b0ce1791dc0faf7cfb5905ba99e35c8df8
alekseynecha77 <68791158+alekseynecha77@users.noreply.github.com>
2022-06-27 20:31:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-const editBTN = document.getElementById('unorderlist');
-
+const editBTN = document.getElementById("unorderlist");
 
 document.getElementById("myBTN").onclick = function () {
-  var textInp = document.getElementById("anything")
+  var textInp = document.getElementById("anything");
 
   var ul = document.getElementById("unorderlist");
 
   var NodeBtn = document.getElementsByTagName("li");
- 
 
   var button = document.createElement("button");
-  var li = document.createElement('li');
-li.innerHTML +=  `<li>${textInp.value}</li>`
+  var li = document.createElement("li");
+  li.innerHTML += `<li>${textInp.value}</li>`;
   button.innerHTML = "edit";
 
   li.appendChild(button);
   ul.appendChild(li);
-
-
 
   for (let i = 0; i < NodeBtn.length; i++) {
     var span = document.createElement("SPAN");
@@ -35,34 +31,33 @@ li.innerHTML +=  `<li>${textInp.value}</li>`
   }
 };
 
-editBTN.addEventListener('click', (e) => {
-  if (e.target.tagName === 'BUTTON') {
+editBTN.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
     const button = e.target;
     const li = button.parentNode;
     const ul = li.parentNode;
-    if (button.textContent === 'remove') {
+
+    if (button.textContent === "remove") {
       ul.removeChild(li);
-    } else if (button.textContent === 'edit') { 
+    } else if (button.textContent === "edit") {
       const span = li.firstElementChild;
-      const input = document.createElement('input');
-      input.type = 'text';
+      const input = document.createElement("input");
+      input.type = "text";
       input.value = span.textContent;
       li.insertBefore(input, span);
       li.removeChild(span);
-      button.textContent = 'save';
-    } else if (button.textContent === 'save') { 
+      button.textContent = "save";
+    } else if (button.textContent === "save") {
       const input = li.firstElementChild;
-      const span = document.createElement('span');
+      const span = document.createElement("span");
       span.textContent = input.value;
       li.insertBefore(span, input);
       li.removeChild(input);
-      button.textContent = 'edit';
+      button.textContent = "edit";
     }
   }
-});  
+});
 
-    
- 
 function myFunction() {
   const list = document.getElementById("unorderlist");
   list.removeChild(list.firstElementChild);
